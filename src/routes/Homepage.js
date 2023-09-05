0de9c3785c0852a676e21a767a0972ae4e04c222
Mohammad Mahdi Niazi <m.mahdi.niazi@gmail.com>
2023-09-05 21:47:19 +0430

    Add Details.js and Homepage.js files

--- src/routes/Homepage.js
+++ src/routes/Homepage.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Navbar from '../components/Navbar/Navbar';
+import Country from '../components/Country/Country';
+import Filter from '../components/Filter/Filter';
 
 const Homepage = () => (
-  <div>Homepage</div>
+  <>
+    <Navbar title="Countries" back={false} />
+    <Filter />
+    <Country />
+  </>
 );
 
 export default Homepage;
